48846f325d02fa31975de03c065a2fd14643c4e1
Rosi <rouvermanuela@gmail.com>
2026-05-29 20:02:49 -0400

    more

--- Assets/scripts/Assignment1_Traffic.cs
+++ Assets/scripts/Assignment1_Traffic.cs
@@ -3,9 +3,10 @@ using UnityEngine.InputSystem;
 
 public class Assignment1_Traffic : MonoBehaviour
 {
+    public float MaxSpeed;
     public float speed;
-    public float duration;
-    float progress = 0f;
+    public float MinSpeed;
+    public float lerpSpeed;
     public float resetPos;
     public float mouseDistance;
     public Camera gameCamera;
@@ -14,38 +15,45 @@ public class Assignment1_Traffic : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
+       
         Vector2 currentPosition = transform.position;
-        currentPosition.y += speed * Time.deltaTime;
+        currentPosition.y += MaxSpeed * Time.deltaTime;
         transform.position = currentPosition;
 
         progress += Time.deltaTime;
 
-       
+      
 
-        if(currentPosition.y > resetPos)
+        Vector2 currentMousePosition = Mouse.current.position.ReadValue();
+      
+
+        Vector2 worldMousePosition = gameCamera.ScreenToWorldPoint(currentMousePosition);
+       // Debug.Log(worldMousePosition);
+        float distance = Vector2.Distance(currentPosition, worldMousePosition);
+
+
+        if (distance < mouseDistance)
+        {
+            //speed = 0f;
+            speed = Mathf.Lerp(MaxSpeed, MinSpeed, lerpSpeed * Time.deltaTime);
+            Debug.Log("Near");
+        }
+        else
+        {
+            speed = Mathf.Lerp(speed, MaxSpeed, lerpSpeed * Time.deltaTime);
+        }
+
+        if (currentPosition.y > resetPos)
         {
             currentPosition.y = -6.94f;
         }
 
         transform.position = currentPosition;
-
-        Vector2 currentMousePosition = Mouse.current.position.ReadValue();
-       
-        Vector2 worldMousePosition = gameCamera.ScreenToWorldPoint(currentMousePosition);
-       // Debug.Log(worldMousePosition);
-        float distance = Vector2.Distance(currentPosition, worldMousePosition);
-     
-        Debug.Log(distance);
-
-        if (distance < mouseDistance)
-        {
-           Debug.Log("Near");
-        }
     }
 }
