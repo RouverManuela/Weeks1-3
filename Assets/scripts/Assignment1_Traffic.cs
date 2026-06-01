1bec2887bf7a38fe892cf847debd577842ad2674
Rosi <rouvermanuela@gmail.com>
2026-06-01 14:59:50 -0400

    comments

--- Assets/scripts/Assignment1_Traffic.cs
+++ Assets/scripts/Assignment1_Traffic.cs
@@ -29,45 +29,45 @@ public class Assignment1_Traffic : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       
+       //Changung the car's current position 
         Vector2 currentPosition = transform.position;
         currentPosition.y += speed * Time.deltaTime;
         transform.position = currentPosition;
 
+        //Getting and converting the mouse's position into world space (in order to allow the game to detect it)
         Vector2 currentMousePosition = Mouse.current.position.ReadValue();
-
         Vector2 worldMousePosition = gameCamera.ScreenToWorldPoint(currentMousePosition);
-       // Debug.Log(worldMousePosition);
+       //checks mouse's distance from object
         float distance = Vector2.Distance(currentPosition, worldMousePosition);
 
-
+        //If the mouse is near the car it slows it down till it stops Speed= maxSpeed----lerpSpeed----minSpeed(0)
         if (distance < mouseDistance)
         {
             //speed = 0f;
             speed = Mathf.Lerp(speed, MinSpeed, lerpSpeed * Time.deltaTime);
            
         }
+        //otherwise, cars keep going
         else
         {
             speed = Mathf.Lerp(speed, MaxSpeed, lerpSpeed * Time.deltaTime);
         }
-
+        //Once the car exits the camera view they reset and teleport down
         if (currentPosition.y > resetPos)
         {
+            //lower than camera to give the impression of time passing 
             currentPosition.y = -6.94f;
             Debug.Log("jump");
         }
-
+        //Animation curve progress
         progress += Time.deltaTime;
 
+        //Lets car shake side to side 
         if (progress > duration)
         {
             progress = 0f;
         }
         currentPosition.x = minShake + engine.Evaluate(progress / duration) * maxShake;
-
-
-
 
         transform.position = currentPosition;
     }
