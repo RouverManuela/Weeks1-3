ff23348b845fd0089eaf6164aca79786d1f67c42
Rosi <rouvermanuela@gmail.com>
2026-06-01 18:04:36 -0400

    .

--- Assets/scripts/Assignment1_Traffic.cs
+++ Assets/scripts/Assignment1_Traffic.cs
@@ -73,11 +73,11 @@ public class Assignment1_Traffic : MonoBehaviour
     }
 }
 
-////////////////////////////////////////////////////////Inspector Values///////////////////////////////////////////////////////////////////
-///// Blue Car- MaxSpeed(3), MinSpeed(0), LerpSpeed(1.5), ResetPos(6.94), MouseDistance(2.5), MinShake(-0.81), MaxSpeed(-0.82), Duration(1)
-///// Red Car- MaxSpeed(4.5), MinSpeed(0), LerpSpeed(2), RestePos(7), MouseDistance(2.5), MinShake(0.32), MaxSpeed(0.33), Duration(1)
-///// Orange Car- MaxSpeed(4.5), MinSpeed(0), LerpSpeed(2), RestePos(7), MouseDistance(2.5), MinShake(1.2), MaxSpeed(1.21), Duration(1)
-///// Green Car- MaxSpeed(3.5), MinSpeed(0), LerpSpeed(2), RestePos(6.56), MouseDistance(-2.17), MinShake(-2.18), MaxSpeed(0.33), Duration(1)
+////////////////////////////////////////////////////////Inspector Values//////////////////////////////////////////////////////////////////////
+// Blue Car- MaxSpeed(3), MinSpeed(0), LerpSpeed(1.5), ResetPos(6.94), MouseDistance(2.5), MinShake(-0.81), MaxSpeed(-0.82), Duration(1)  ////
+// Red Car- MaxSpeed(4.5), MinSpeed(0), LerpSpeed(2), RestePos(7), MouseDistance(2.5), MinShake(0.32), MaxSpeed(0.33), Duration(1)  /////////
+// Orange Car- MaxSpeed(4.5), MinSpeed(0), LerpSpeed(2), RestePos(7), MouseDistance(2.5), MinShake(1.2), MaxSpeed(1.21), Duration(1)  ///////
+// Green Car- MaxSpeed(3.5), MinSpeed(0), LerpSpeed(2), RestePos(6.56), MouseDistance(-2.17), MinShake(-2.18), MaxSpeed(0.33), Duration(1)///
 
 
 
