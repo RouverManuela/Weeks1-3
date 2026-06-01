a4d2c99ea317bc53f71b2f66603862c16c991d5d
Rosi <rouvermanuela@gmail.com>
2026-06-01 14:54:23 -0400

    final changes

--- Assets/scripts/Assignment1_Traffic.cs
+++ Assets/scripts/Assignment1_Traffic.cs
@@ -3,19 +3,27 @@ using UnityEngine.InputSystem;
 
 public class Assignment1_Traffic : MonoBehaviour
 {
+    //Speed and movement
     public float MaxSpeed;
-    public float speed;
     public float MinSpeed;
     public float lerpSpeed;
     public float resetPos;
     public float mouseDistance;
     public Camera gameCamera;
+    float speed;
+
+    //Shaking animation
+    public AnimationCurve engine;
+    public float minShake;
+    public float maxShake;
+    float progress = 0f;
+    public float duration;
    
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-       
+        speed = MaxSpeed;
     }
 
     // Update is called once per frame
@@ -23,15 +31,10 @@ public class Assignment1_Traffic : MonoBehaviour
     {
        
         Vector2 currentPosition = transform.position;
-        currentPosition.y += MaxSpeed * Time.deltaTime;
+        currentPosition.y += speed * Time.deltaTime;
         transform.position = currentPosition;
 
-        progress += Time.deltaTime;
-
-      
-
         Vector2 currentMousePosition = Mouse.current.position.ReadValue();
-      
 
         Vector2 worldMousePosition = gameCamera.ScreenToWorldPoint(currentMousePosition);
        // Debug.Log(worldMousePosition);
@@ -41,8 +44,8 @@ public class Assignment1_Traffic : MonoBehaviour
         if (distance < mouseDistance)
         {
             //speed = 0f;
-            speed = Mathf.Lerp(MaxSpeed, MinSpeed, lerpSpeed * Time.deltaTime);
-            Debug.Log("Near");
+            speed = Mathf.Lerp(speed, MinSpeed, lerpSpeed * Time.deltaTime);
+           
         }
         else
         {
@@ -52,7 +55,19 @@ public class Assignment1_Traffic : MonoBehaviour
         if (currentPosition.y > resetPos)
         {
             currentPosition.y = -6.94f;
+            Debug.Log("jump");
         }
+
+        progress += Time.deltaTime;
+
+        if (progress > duration)
+        {
+            progress = 0f;
+        }
+        currentPosition.x = minShake + engine.Evaluate(progress / duration) * maxShake;
+
+
+
 
         transform.position = currentPosition;
     }
